fbb8efafc7f39851c77b3b0bf583fc762db30311
hgwayxihuan <2692677365@qq.com>
2021-04-27 15:19:16 +0800

    C语言

---
+++ 2021-4-18.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		i *= n;
+//	}
+//	printf("%dµÄ½×³ËÊÇ%d", n, i);
+//	
+//	return 0;
+//}
