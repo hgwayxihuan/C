167043e0e3dd54ffb19ddeea1e50f47c75397455
hgwayxihuan <2692677365@qq.com>
2021-04-27 15:49:51 +0800

    C语言练习

---
+++ 2021-4-20.c
@@ -0,0 +1,39 @@
+//#include<stdio.h>
+//void print(int x)
+//{
+//	if (x > 9)
+//	{
+//		print(x / 10);
+//
+//	}
+//	printf("%d ", x%10);
+//
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	print(a);
+//
+//	return 0;
+//
+//}
+//int Fibonacci(int n) {
+//
+//    if (n <= 2)
+//    {
+//        return 1;
+//    }
+//    else
+//        return Fibonacci(n - 1) + Fibonacci(n - 2);
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ret = 0;
+//    ret = Fibonacci(n);
+//    printf("%d", ret);
+//    return 0;
+//}
