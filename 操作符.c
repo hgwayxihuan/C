4be428c2d8aeb292fd032949a826fa99538164c4
hgwayxihuan <2692677365@qq.com>
2021-05-12 21:50:34 +0800

    操作符.c

---
+++ 操作符.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 123;
+//	char b = 200;
+//	char c = a + b;
+//	printf("%d", c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char c = 1;
+//	printf("%u\n", sizeof(c));
+//	printf("%u\n", sizeof(+c));
+//	printf("%u\n", sizeof(!c));
+//	return 0;
+//}
