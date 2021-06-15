34b7ab110f6209832bb89ce4594a221913ab1455
hgwayxihuan <2692677365@qq.com>
2021-06-15 22:28:26 +0800

    指针笔试题2.c

---
+++ 指针笔试题2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//7
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+//8
+int main()
+{
+	char* c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+	char** cp[] = { c + 3, c + 2, c + 1, c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
